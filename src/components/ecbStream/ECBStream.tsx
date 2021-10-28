277f5015a9d7873c2abf075b7b1697ccd8232d37
Dirk van der Straaten <dirk.vanderstraaten@gmail.com>
2021-10-28 14:16:11 +0200

    adds some finetuning tot ECBStream

--- src/components/ecbStream/ECBStream.tsx
+++ src/components/ecbStream/ECBStream.tsx
@@ -2,13 +2,6 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import * as d3 from "d3";
 import styles from "./ECBStream.module.scss";
 
-const margin = { top: 10, right: 30, bottom: 30, left: 60 };
-const widthDesktop = 640 - margin.left - margin.right;
-const heightDesktop = 600 - margin.top - margin.bottom;
-const widthMobile = 360 - margin.left - margin.right;
-const heightMobile = 250 - margin.top - margin.bottom;
-const dates = ["1/1/2009", "1/2/2012", "30/10/2014", "15/3/2020", "19/10/2021"];
-
 export interface Props {
   currentPart: string;
   isMobileSize: boolean;
@@ -16,6 +9,12 @@ export interface Props {
   active: boolean;
 }
 
+const margin = { top: 10, right: 30, bottom: 30, left: 60 };
+const widthDesktop = 640 - margin.left - margin.right;
+const heightDesktop = 600 - margin.top - margin.bottom;
+const widthMobile = 360 - margin.left - margin.right;
+const heightMobile = 500 - margin.top - margin.bottom;
+const dates = ["1/9/2008", "1/2/2012", "1/11/2014", "1/3/2020", "1/10/2021"];
 const parseYear = d3.timeParse("%Y");
 const parseWeek = d3.timeParse("%d/%m/%Y");
 const x = d3.scaleLinear().domain([0, 8000000]);
@@ -41,12 +40,7 @@ function ECBStream(props: Props) {
     const color = d3
       .scaleOrdinal()
       .domain(keys)
-      .range([
-        "var(--writer)",
-        "var(--captions)",
-        "#4C4642",
-        "var(--input-caret",
-      ]);
+      .range(["#A6988F", "#CDBEB4", "#4C4642", "#41AFB0"]);
 
     d3.select(ECBRef.current)
       .select("svg")
@@ -60,6 +54,7 @@ function ECBStream(props: Props) {
       })
       .style("opacity", "0");
 
+    addLegenda();
     props.currentPart
       ? updateAreas(parseInt(props.currentPart.slice(0)) - 1)
       : "";
@@ -67,37 +62,166 @@ function ECBStream(props: Props) {
 
   const updateAreas = useCallback(
     (dateIndex) => {
-      const u = d3
-        .select(ECBRef.current)
-        .select("svg")
-        .select("g")
-        .selectAll(".area")
-        .data(stackedData);
+      if (keys) {
+        const u = d3
+          .select(ECBRef.current)
+          .select("svg")
+          .select("g")
+          .selectAll(".area")
+          .data(stackedData);
 
-      u.join("path")
-        .transition()
-        .duration(1000)
-        .attr(
-          "d",
-          d3
-            .area()
-            .y((d) => {
-              return parseWeek(dates[dateIndex]) > parseWeek(d.data.Week)
-                ? y(parseWeek(d.data.Week))
-                : y(parseWeek(dates[dateIndex]));
-            })
-            .x0((d) => {
-              return x(d[0]);
-            })
-            .x1((d) => {
-              return x(d[1]);
-            })
-            .curve(d3.curveBasis)
-        )
-        .style("opacity", "1");
+        u.join("path")
+          .transition()
+          .ease(d3.easeLinear)
+          .duration(300)
+          .attr(
+            "d",
+            d3
+              .area()
+              .y((d) => {
+                return parseWeek(dates[dateIndex]) > parseWeek(d.data.Week)
+                  ? y(parseWeek(d.data.Week))
+                  : y(parseWeek(dates[dateIndex]));
+              })
+              .x0((d) => {
+                return x(d[0]);
+              })
+              .x1((d) => {
+                return x(d[1]);
+              })
+              .curve(d3.curveBasis)
+          )
+          .style("opacity", "1");
+
+        d3.selectAll("text.legenda_amount")
+          .data(keys)
+          .text((d) => {
+            return `${(
+              ECBData3.filter((element) => element.Week == dates[dateIndex])[0][
+                d
+              ] / 1000
+            ).toFixed()} ${props.isMobileSize ? "" : "miljard"}`;
+          });
+      }
     },
-    [ECBRef, stackedData, dates]
+    [ECBRef, stackedData, dates, keys, ECBData3, props.isMobileSize]
   );
+
+  const addLegenda = useCallback(() => {
+    if (keys) {
+      const size = 20;
+      const labelSvg = d3.select(ECBRef.current).select("svg");
+      labelSvg
+        .append("defs")
+        .append("linearGradient")
+        .attr("x1", "0%")
+        .attr("y1", "0%")
+        .attr("x2", "0%")
+        .attr("y2", "100%")
+        .attr("id", "myGradient")
+        .append("stop")
+        .attr("offset", "70%")
+        .attr("stop-color", "var(--product-background-2)")
+        .attr("stop-opacity", 1);
+
+      labelSvg
+        .select("#myGradient")
+        .append("stop")
+        .attr("offset", "100%")
+        .attr("stop-color", "var(--product-background-2)")
+        .attr("stop-opacity", 0);
+
+      labelSvg
+        .append("rect")
+        .attr(
+          "x",
+          props.isMobileSize
+            ? widthMobile + margin.left + margin.right - widthMobile * 0.8
+            : widthDesktop + margin.left + margin.right - widthDesktop * 0.8
+        )
+        .attr("y", 0)
+        .attr(
+          "width",
+          props.isMobileSize ? widthMobile * 0.8 : widthDesktop * 0.8
+        )
+        .attr("height", 150)
+        .style("fill", "url(#myGradient)");
+
+      labelSvg
+        .selectAll("mydots")
+        .data(keys)
+        .join("rect")
+        .attr(
+          "x",
+          props.isMobileSize
+            ? widthMobile + margin.right + margin.left - size
+            : widthDesktop + margin.right + margin.left - size
+        )
+        .attr("y", (_, i) => {
+          return margin.top + i * (size + 4);
+        })
+        .attr("width", size)
+        .attr("height", size)
+        .attr("class", "legenda")
+        .style("fill", (d) => {
+          switch (d) {
+            case "Goud":
+              return "#A6988F";
+            case "Overig":
+              return "#CDBEB4";
+            case "Bankkredieten":
+              return "#4C4642";
+            case "Opkoopprogramma's":
+              return "#41AFB0";
+          }
+        })
+        .style("opacity", 1);
+
+      labelSvg
+        .selectAll("mylabels")
+        .data(keys)
+        .join("text")
+        .attr(
+          "x",
+          props.isMobileSize
+            ? widthMobile - (size + 2) + margin.right + margin.left
+            : widthDesktop - (size + 2) + margin.right + margin.left
+        )
+        .attr("y", (_, i) => {
+          return margin.top + (i + 1) * (size + 3) - 4;
+        })
+        .text("")
+        .attr("text-anchor", "end")
+        .attr("class", "legenda_amount")
+        .style("font-family", "ProximaNovaRegular")
+        .style("font-size", () => {
+          return props.isMobileSize ? "14px" : "18px";
+        })
+        .style("fill", "var(--content-copy)");
+
+      labelSvg
+        .selectAll("mylabels")
+        .data(keys)
+        .join("text")
+        .attr(
+          "x",
+          props.isMobileSize
+            ? widthMobile - (size + 2) - 48 + margin.right + margin.left
+            : widthDesktop - (size + 2) - 104 + margin.right + margin.left
+        )
+        .attr("y", (_, i) => {
+          return margin.top + (i + 1) * (size + 3) - 4;
+        })
+        .text((d) => `${d}:`)
+        .attr("text-anchor", "end")
+        .attr("class", "legenda_item")
+        .style("font-family", "ProximaNovaRegular")
+        .style("font-size", () => {
+          return props.isMobileSize ? "14px" : "18px";
+        })
+        .style("fill", "var(--content-copy)");
+    }
+  }, [keys, props.isMobileSize]);
 
   useEffect(() => {
     if (ECBRef.current && props.active) {
@@ -111,7 +235,7 @@ function ECBStream(props: Props) {
         });
       }
 
-      if (ECBData3 && keys && stackedData) {
+      if (keys && stackedData) {
         const svg = d3
           .select(ECBRef.current)
           .append("svg")
@@ -134,7 +258,7 @@ function ECBStream(props: Props) {
           .append("g")
           .attr(
             "transform",
-            `translate(${props.isMobileSize ? margin.left + 40 : margin.left},${
+            `translate(${props.isMobileSize ? margin.left : margin.left},${
               margin.top
             })`
           );
@@ -143,6 +267,7 @@ function ECBStream(props: Props) {
         x.range([0, props.isMobileSize ? widthMobile : widthDesktop]);
         svg
           .append("g")
+          .attr("class", "x axis-grid")
           .attr(
             "transform",
             `translate(0, ${props.isMobileSize ? heightMobile : heightDesktop})`
@@ -151,22 +276,40 @@ function ECBStream(props: Props) {
             d3
               .axisBottom(x)
               .ticks(5)
-              .tickFormat((d: number) => d / 1000000)
+              .tickSize(-`${props.isMobileSize ? heightMobile : heightDesktop}`)
+              .tickFormat((d: number) => d / 1000)
+            // .attr("stroke", "var(--product-background-2)")
           );
 
         // Add Y axis
         y.range([props.isMobileSize ? heightMobile : heightDesktop, 0]);
-        svg.append("g").call(d3.axisLeft(y).tickFormat(d3.timeFormat("%Y")));
+        svg
+          .append("g")
+          // .attr("class", "y")
+          .call(d3.axisLeft(y).tickFormat(d3.timeFormat("%Y")));
 
         svg
           .selectAll(".tick")
           .selectAll("text")
           .attr("class", "body-text sans s")
           .attr("color", "var(--content-copy)");
+
+        svg.select(".x.axis-grid").select("path.domain").attr("stroke", "none");
+        svg
+          .select(".x.axis-grid")
+          .selectAll(".tick")
+          .selectAll("line")
+          .attr("stroke", "#4C4642");
+
+        svg
+          .select(".y")
+          .selectAll(".tick")
+          .selectAll("line")
+          .attr("stroke", "none");
         showAreas();
       }
     }
-  }, [props.isMobileSize, props.active, keys, stackedData, ECBData3]);
+  }, [props.isMobileSize, props.active, keys, stackedData]);
 
   useEffect(() => {
     const cpIndex = props.currentPart
